5ae657b7fe1d9f4d38b407a088c407e55f21a4a0
Juliana Gentile <90779400+julianagentile@users.noreply.github.com>
2025-04-14 12:24:06 -0400

    Update Lab8_main.c

--- Lab8_main.c
+++ Lab8_main.c
@@ -21,7 +21,13 @@
 // change to Heap_Size       EQU     0x00000200 in startup_msp432p401r_uvision.s
 
 #define ADC_MAX 16384.0   // 14-bit ADC resolution
-#define VREF 3.3          // Reference voltage in Volts
+#define VREF 2.5          // Reference voltage in Volts
+
+//#define THRESHOLD_VOLTAGE 1.2  // Adjust based on your sensor output
+#define MIN_HEARTBEAT_INTERVAL_MS 300  // Min time between peaks (bpm < 200)
+
+#define MAX_BPM 	240
+#define MIN_BPM		60
 
 BOOLEAN Timer1RunningFlag = FALSE;
 
@@ -32,7 +38,13 @@ extern uint32_t SystemCoreClock;
 volatile unsigned int adc_heartrate = 0;
 volatile BOOLEAN new_heartbeat_data = FALSE;
 
-double voltage, heartrate_bpm;
+double voltage;
+int heartrate_bpm;
+
+unsigned long last_beat_time = 0;
+unsigned long current_time = 0;
+
+int isPeak = 0;
 
 
 // Interrupt Service Routine for Timer32-1
@@ -40,6 +52,7 @@ void Timer32_1_ISR(void)
 {
     adc_heartrate = ADC_In(); // Read ADC value from channel A6 (P4.7)
     new_heartbeat_data = TRUE;
+		MillisecondCounter++;
 }
 
 
@@ -56,8 +69,7 @@ void PORT1_IRQHandler(void) // main purpose is to see where the interrupt came f
 // main
 int main(void)
 {
-	char temp[64];
-	unsigned int analogIn = 0;
+	char heartrate[64];
 	//initializations
 	uart0_init();
 	uart0_put("\r\nLab8 Heartbeat Demo\r\n");
@@ -67,19 +79,35 @@ int main(void)
 	Timer32_1_Init(&Timer32_1_ISR, SystemCoreClock/2, T32DIV1); // initialize Timer A32-1;
 
 	EnableInterrupts();
+	current_time = 0;
+	int adcvals[3];
+	int beat_count = 0;
+	int adc_peak = 0;
+	
   while(1)
 	{
 		for (;;) {
-			if (new_heartbeat_data) {
-				new_heartbeat_data = FALSE;
+			isPeak = 0;
+			while(!isPeak){
+				if(new_heartbeat_data) {
+					adcvals[2] = adcvals[1];
+					adcvals[1] = adcvals[0];
+					adcvals[0] = adc_heartrate;
+					new_heartbeat_data = FALSE;
+					if (adcvals[0] < adcvals[1] && adcvals[1] > adcvals[2]) {
+						isPeak = 1;
+						adc_peak = adcvals[1];
+						beat_count++;
+					}
+				}
+			}		
 				
-				// Convert ADC value to heart rate
-				voltage = (adc_heartrate / ADC_MAX) * VREF;
-				//temperature_C = (voltage * 100.0 - 50.0) / 1.8;
+			// Convert ADC value to heart rate
+			voltage = (adc_peak / ADC_MAX) * VREF;	
+			heartrate_bpm = (voltage / VREF) * MAX_BPM;
 				
-				sprintf(heartrate, "\r\nHeartrate is %u BPM", adc_heartrate);
-				uart0_put(heartrate);
-			}
-		}
-  }
+			sprintf(heartrate, "\r\nHeartrate is %u BPM", heartrate_bpm);
+			uart0_put(heartrate);
+		}	
+	}
 }
