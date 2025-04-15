475c7c6e0e38f9b6c54b5a8e7baf0a693aae8dd4
Juliana Gentile <90779400+julianagentile@users.noreply.github.com>
2025-04-15 13:01:11 -0400

    Update Lab8_main.c

--- Lab8_main.c
+++ Lab8_main.c
@@ -24,7 +24,7 @@
 #define VREF 2.5          // Reference voltage in Volts
 
 //#define THRESHOLD_VOLTAGE 1.2  // Adjust based on your sensor output
-#define MIN_HEARTBEAT_INTERVAL_MS 300  // Min time between peaks (bpm < 200)
+#define MIN_HEARTBEAT_INTERVAL_MS 200  // Min time between peaks (bpm < 240ish)
 
 #define MAX_BPM 	240
 #define MIN_BPM		60
@@ -39,7 +39,7 @@ volatile unsigned int adc_heartrate = 0;
 volatile BOOLEAN new_heartbeat_data = FALSE;
 
 double voltage;
-int heartrate_bpm;
+double heartrate_bpm;
 
 unsigned long last_beat_time = 0;
 unsigned long current_time = 0;
@@ -79,7 +79,8 @@ int main(void)
 	Timer32_1_Init(&Timer32_1_ISR, SystemCoreClock/2, T32DIV1); // initialize Timer A32-1;
 
 	EnableInterrupts();
-	current_time = 0;
+	unsigned long last_beat_time = 0;
+	unsigned long current_beat_time = 0;	
 	int adcvals[3];
 	int beat_count = 0;
 	int adc_peak = 0;
@@ -95,19 +96,59 @@ int main(void)
 					adcvals[0] = adc_heartrate;
 					new_heartbeat_data = FALSE;
 					if (adcvals[0] < adcvals[1] && adcvals[1] > adcvals[2]) {
-						isPeak = 1;
-						adc_peak = adcvals[1];
-						beat_count++;
+						current_beat_time = MillisecondCounter;
+            unsigned long interval = current_beat_time - last_beat_time;
+
+            //if (interval > (double)(MIN_HEARTBEAT_INTERVAL_MS)/1000.0) {
+							heartrate_bpm = (double)(MAX_BPM) / (double)(interval);
+							//heartrate_bpm = 60000.0 / ((double)(interval));
+							last_beat_time = current_beat_time;
+							isPeak = 1;
+							adc_peak = adcvals[1];
+							beat_count++;
+						//}
+							
+						sprintf(heartrate, "\r\nCurrent Time %lu", current_beat_time);
+							uart0_put(heartrate);
+						
+						sprintf(heartrate, "\r\nLast Time %lu", last_beat_time);
+							uart0_put(heartrate);
+						
+							sprintf(heartrate, "\r\nBeat #%d", beat_count);
+							uart0_put(heartrate);
+
+							sprintf(heartrate, "\r\nInterval: %lu ms", interval);
+							uart0_put(heartrate);
+
+							sprintf(heartrate, "\r\nHeartrate is %u BPM\n", (int)(heartrate_bpm));
+							uart0_put(heartrate);
 					}
 				}
 			}		
 				
 			// Convert ADC value to heart rate
-			voltage = (adc_peak / ADC_MAX) * VREF;	
-			heartrate_bpm = (voltage / VREF) * MAX_BPM;
+			
+			//voltage = (adc_peak / ADC_MAX) * VREF;			
+			//heartrate_bpm = (voltage / VREF) * MAX_BPM;
+			
+			//if (MillisecondCounter * 60000 >= 1) {
+			
+			//sprintf(heartrate, "\r\nMillisecond counter: %lu", MillisecondCounter);
+			//uart0_put(heartrate);
 				
-			sprintf(heartrate, "\r\nHeartrate is %u BPM", heartrate_bpm);
-			uart0_put(heartrate);
+			//sprintf(heartrate, "\r\nBeat count: %u", beat_count);
+			//uart0_put(heartrate);
+			
+			//heartrate_bpm = (MillisecondCounter/beat_count) * 30;
+			//heartrate_bpm = ((double)(beat_count * 60000)) / ((double)(MillisecondCounter));
+
+			
+			//sprintf(heartrate, "\r\n ADC Peak: %u", adc_peak);
+				//uart0_put(heartrate);
+	
+				//sprintf(heartrate, "\r\nHeartrate is %u BPM", (int)(heartrate_bpm));
+				//uart0_put(heartrate);
+			//}
 		}	
 	}
 }
