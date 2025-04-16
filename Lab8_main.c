1744d5488a145a1d5c302502989aa7840701f23d
Juliana Gentile <90779400+julianagentile@users.noreply.github.com>
2025-04-16 14:22:28 -0400

    Final Lab 8 Main

--- Lab8_main.c
+++ Lab8_main.c
@@ -1,14 +1,17 @@
 /*
-* Rochester Institute of Technology
-* Department of Computer Engineering
-* CMPE 460  Interfacing Digital Electronics
-* LJBeato
-* 1/14/2021
-*
-* Filename: main_timer_template.c
-*/
+ * Rochester Institute of Technology
+ * Department of Computer Engineering
+ * CMPE 460 Interfacing Digital Electronics
+ * LJBeato
+ * 1/14/2021
+ *
+ * Modified for heartbeat detection using MSP432
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
 
 #include "msp.h"
 #include "uart.h"
@@ -16,142 +19,88 @@
 #include "CortexM.h"
 #include "Common.h"
 #include "ADC14.h"
-// The sprintf function seemed to cause a hange in the interrupt service routine.
-// I think if we increase the HEAP size, it will work
-// change to Heap_Size       EQU     0x00000200 in startup_msp432p401r_uvision.s
 
-#define ADC_MAX 16384.0   // 14-bit ADC resolution
-#define ADC_THRESHOLD 9830.0
-#define VREF 2.5          // Reference voltage in Volts
+#define ADC_MAX 16384.0     // 14-bit ADC resolution
+#define ADC_THRESHOLD 9830  // Approx 60% of full scale
+#define VREF 2.5            // ADC reference voltage in Volts
 
-//#define THRESHOLD_VOLTAGE 1.2  // Adjust based on your sensor output
-#define MIN_HEARTBEAT_INTERVAL_MS 200  // Min time between peaks (bpm < 240ish)
+#define MIN_HEARTBEAT_INTERVAL_MS 200  // Minimum time between peaks (in ms)
 
-#define MAX_BPM 	240
-#define MIN_BPM		60
-
-BOOLEAN Timer1RunningFlag = FALSE;
-
+volatile uint32_t adc_sample = 0;
+volatile bool new_adc_data = false;
 unsigned long MillisecondCounter = 0;
 
-extern uint32_t SystemCoreClock;
-
-volatile unsigned int adc_heartrate = 0;
-volatile BOOLEAN new_heartbeat_data = FALSE;
-
-double voltage;
-double heartrate_bpm;
+typedef enum {
+    BELOW_THRESHOLD,
+    ABOVE_THRESHOLD
+} PeakState;
 
 unsigned long last_beat_time = 0;
-unsigned long current_time = 0;
+unsigned long current_beat_time = 0;
+double heartrate_bpm = 0;
+int beat_count = 0;
 
-int isPeak = 0;
-
-
-// Interrupt Service Routine for Timer32-1
+// ISR for 1ms Timer32 interrupt
 void Timer32_1_ISR(void)
 {
-    adc_heartrate = ADC_In(); // Read ADC value from channel A6 (P4.7)
-    new_heartbeat_data = TRUE;
-		MillisecondCounter++;
+    adc_sample = ADC_In();         // Read ADC from channel A6 (P4.7)
+    new_adc_data = true;           // Signal new data
+    MillisecondCounter++;          // Track time in milliseconds
 }
 
-
-void PORT1_IRQHandler(void) // main purpose is to see where the interrupt came from and we can handle it respective values. 
+// ISR for button on P1.1 (toggle sampling)
+void PORT1_IRQHandler(void)
 {
-	if(P1->IFG & BIT1)
-	{
+    if (P1->IFG & BIT1) {
         P1->IFG &= ~BIT1;
-        Timer1RunningFlag = !Timer1RunningFlag;
-  }
-}	
+    }
+}
 
-
-// main
 int main(void)
 {
-	char heartrate[64];
-	//initializations
-	uart0_init();
-	uart0_put("\r\nLab8 Heartbeat Demo\r\n");
+    char output[64];
+    uart0_init();
+    uart0_put("\r\nHeartbeat Detection (MSP432)\r\n");
 
-	ADC0_InitSWTriggerCh6();
+    ADC0_InitSWTriggerCh6();  // Initialize ADC (P4.7)
+    Timer32_1_Init(&Timer32_1_ISR, SystemCoreClock / 1000, T32DIV1);  // 1ms ticks
 
-	Timer32_1_Init(&Timer32_1_ISR, SystemCoreClock/2, T32DIV1); // initialize Timer A32-1;
+    EnableInterrupts();
 
-// take adc value array, make it bigger (large enough to accomodate slowest frequency).
-// Sample adc and fill that array
-// 
-	
-	EnableInterrupts();
-	double last_beat_time = 0.0;
-	double current_beat_time = 0.0;	
-	int adcvals[3];
-	int beat_count = 0;
-	int adc_peak = 0;
-	
-		for (;;) {
-			isPeak = 0;
-			while(!isPeak){
-				if(new_heartbeat_data) {
-					adcvals[2] = adcvals[1];
-					adcvals[1] = adcvals[0];
-					adcvals[0] = adc_heartrate;
-					new_heartbeat_data = FALSE;
-					//if (adcvals[0] < adcvals[1] && adcvals[1] > adcvals[2]) {
-					if(adcvals[1] > ADC_THRESHOLD && adcvals[1] > adcvals[0]){
-						current_beat_time = MillisecondCounter/1000.0;
-            double interval = (current_beat_time - last_beat_time);
+    int prev = 0, curr = 0, next = 0;
+    PeakState state = BELOW_THRESHOLD;
 
-            //if (interval > (MIN_HEARTBEAT_INTERVAL_MS)/1000.0) {
-							heartrate_bpm = (double)(MAX_BPM) / (double)(interval*2000.0);
-							//heartrate_bpm = 60.0 / ((double)(interval*1000.0));
-						sprintf(heartrate, "\r\nLast Time %f", last_beat_time);
-						uart0_put(heartrate);
-							last_beat_time = current_beat_time;
-							isPeak = 1;
-							adc_peak = adcvals[1];
-							beat_count++;
-						//}
-							
-						sprintf(heartrate, "\r\nCurrent Time %f", current_beat_time);
-						uart0_put(heartrate);
-						
-							sprintf(heartrate, "\r\nBeat #%d", beat_count);
-							uart0_put(heartrate);
+    while (1) {
+        if (new_adc_data) {
+            new_adc_data = false;
 
-							sprintf(heartrate, "\r\nInterval: %f ms", interval);
-							uart0_put(heartrate);
+            // Shift ADC samples
+            next = curr;
+            curr = prev;
+            prev = adc_sample;
 
-							sprintf(heartrate, "\r\nHeartrate is %u BPM\n", (int)(heartrate_bpm));
-							uart0_put(heartrate);
-					}
-				}
-			}		
-				
-			// Convert ADC value to heart rate
-			
-			//voltage = (adc_peak / ADC_MAX) * VREF;			
-			//heartrate_bpm = (voltage / VREF) * MAX_BPM;
-			
-			//if (MillisecondCounter * 60000 >= 1) {
-			
-			//sprintf(heartrate, "\r\nMillisecond counter: %lu", MillisecondCounter);
-			//uart0_put(heartrate);
-				
-			//sprintf(heartrate, "\r\nBeat count: %u", beat_count);
-			//uart0_put(heartrate);
-			
-			//heartrate_bpm = (MillisecondCounter/beat_count) * 30;
-			//heartrate_bpm = ((double)(beat_count * 60000)) / ((double)(MillisecondCounter));
+            // Detect peak using a state machine
+            if (state == BELOW_THRESHOLD) {
+                if (curr > ADC_THRESHOLD && curr >= prev && curr >= next) {
+                    current_beat_time = MillisecondCounter;
 
-			
-			//sprintf(heartrate, "\r\n ADC Peak: %u", adc_peak);
-				//uart0_put(heartrate);
-	
-				//sprintf(heartrate, "\r\nHeartrate is %u BPM", (int)(heartrate_bpm));
-				//uart0_put(heartrate);
-			//}
-		}	
-	
+                    if ((current_beat_time - last_beat_time) > MIN_HEARTBEAT_INTERVAL_MS) {
+                        double interval = (current_beat_time - last_beat_time) / 1000.0;
+                        heartrate_bpm = 60.0 / interval;
+                        beat_count++;
+
+                        sprintf(output, "\r\nBeat #%d at %lu ms", beat_count, current_beat_time);
+                        uart0_put(output);
+                        sprintf(output, "\r\nHeart Rate: %.2f BPM", heartrate_bpm);
+                        uart0_put(output);
+
+                        last_beat_time = current_beat_time;
+                        state = ABOVE_THRESHOLD;  // Wait for drop
+                    }
+                }
+            } else if (curr < ADC_THRESHOLD) {
+                state = BELOW_THRESHOLD;
+            }
+        }
+    }
 }
